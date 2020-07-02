cf10902e2887290c1d88ccf0cbf34064505583b4
digitpk <webmaster@digit.com.pk>
2020-07-02 14:08:51 +0500

    my Learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ldf = 1;
+  int rdf = 6;
+
+  void rollingDice() {
+    ldf = Random().nextInt(6) + 1; //Random function
+    rdf = Random().nextInt(6) + 1; //Random function
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rollingDice();
+                });
+              },
+              child: Image.asset('images/dice$ldf.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rollingDice();
+                });
+              },
+              child: Image.asset('images/dice$rdf.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
